7ac2b00dea3bdb76620ac5f8485c5bc26f1b7e1d
Olena Derevianko <olenka.derevianko@gmail.com>
2022-09-25 20:12:59 +0200

    add files

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+const navEl = document.querySelectorAll('.item');
+console.log(`Number of categories: ${navEl.length}`);
+
+//Category: Animals
+//Elements: 4
+
+// const text = document.querySelectorAll('h2');
+// console.log(text.outerText); // text inside p.article-text
+
+//Category: Products
+//Elements: 3
+
+//Category: Technologies
+//Elements: 5
+
