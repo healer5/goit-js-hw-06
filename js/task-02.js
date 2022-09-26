d8c46ad9a47695283ceae9bb4f097a126209a377
Olena Derevianko <olenka.derevianko@gmail.com>
2022-09-26 18:53:10 +0200

    add task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const list = document.querySelector('#ingredients')
+const items = ingredients.map(item => {
+    const li = document.createElement('li');
+    li.classList.add('item');
+    li.textContent = item;
+    return li;
+})
+
+list.append(...items);
